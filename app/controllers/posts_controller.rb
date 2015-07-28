4b86ed3e809d40f6dea6e476e06f45ab638589b7
raisedfist <barmastare@gmail.com>
2015-07-28 15:34:37 +0200

    form for edit and new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -17,10 +17,28 @@ class PostsController < ApplicationController
   end
   
   def show
-    @post = Post.find(params[:id])
+   
     
   end
 
+  def edit
+   
+    
+  end
+
+  def update
+    
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+    
+    
+  end
+  
+  def destroy
+   
+    @post.destroy
+    redirect_to(posts_path)
+  end
 
 end
 
@@ -31,3 +49,7 @@ end
     
   end
   
+  def set_post
+    @post = Post.find(params[:id])
+    
+  end
