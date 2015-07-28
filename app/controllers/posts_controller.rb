b4580cb4d3acd30bfc6b26b1c6546b402e16819c
raisedfist <barmastare@gmail.com>
2015-07-28 16:50:11 +0200

    added flash helper and fixed error messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,14 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Your post has successfully been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
+    end
   end
   
   def show
@@ -28,15 +34,19 @@ class PostsController < ApplicationController
 
   def update
     
-    @post.update(post_params)
-    redirect_to(post_path(@post))
-    
-    
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post updated"
+      redirect_to(post_path(@post))
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
   end
   
   def destroy
    
     @post.destroy
+    flash[:success] = "Post deleted"
     redirect_to(posts_path)
   end
 
